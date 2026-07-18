9a43886b00b58f072eaf40ae8233ebf97bd24c08
unknown <steven@eddies.me.uk>
2026-07-18 12:46:05 +0100

    feat: add fixed text display

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ class IssTrackerApp(app.App):
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
             self.button_states.clear()
             self.minimise()
-        if self.button_states.get(BUTTON_TYPES["RIGHT"]):
-            self.button_states.clear()
-            self.counter = self.counter + 1
 
     def draw(self, ctx):
-        ctx.rgb(0, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
-        ctx.rgb(0, 1, 0).move_to(-80, 0).text(str(self.counter))
+        ctx.text_align = ctx.CENTER
+        ctx.font_size = 25
+        ctx.rgb(0.2, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
+        ctx.rgb(0.6, 0, 1).move_to(0, -40).text("Next ISS pass in")
 
 __app_export__ = IssTrackerApp
